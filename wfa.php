6af91e55f7e1d28e7ccf7049b135153acd722244
Hotwheels93 <38242626+Hotwheels93@users.noreply.github.com>
2022-01-16 13:25:07 +0100

    Update wfa.php
    fix modified page structure

--- wfa.php
+++ wfa.php
@@ -1,13 +1,13 @@
 <?php
  error_reporting(E_ERROR | E_PARSE);
- $URL = "https://wohlstandfueralle.podigee.io/archive";
+ $URL = "https://audionow.de/podcast/18602779-02d6-4bc0-a237-c6cbb0bc41bb";
  $str = file_get_contents($URL);
  $dom = new DOMDocument;
  $dom->loadHTML($str);
- foreach ($dom->getElementsByTagName('a') as $tag) {
-        if(strpos($tag->getAttribute('href'),'.mp3')!==false)
+ foreach ($dom->getElementsByTagName('div') as $tag) {
+        if(strpos($tag->getAttribute('data-audiolink'),'.mp3')!==false)
         {
-            $url_cunstructor = $tag->getAttribute('href');
+            $url_cunstructor = $tag->getAttribute('data-audiolink');
 			$file_url = explode('?v',$url_cunstructor); 
 			echo ($file_url[0]);
             break;	
